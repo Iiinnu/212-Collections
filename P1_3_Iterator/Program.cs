ababc212e3db725e511d68889b7192a3991ab8c3
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-05-02 13:21:44 +0200

    Setting up Iterator assignment and trying different things

---
+++ P1_3_Iterator/Program.cs
@@ -0,0 +1,15 @@
+﻿using.System.Collections.IEnumerable
+
+
+List<int> number = new List<int>();
+number.Add(1);
+number.Add(2);
+number.Add(3);
+number.Add(4);
+number.Add(5);
+
+
+public interface IEnumerator
+{
+   
+}
