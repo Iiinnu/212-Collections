1f258e2532db8aba66e50fa5c28d649ae123f51a
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-05-02 13:57:45 +0200

    Assignment Iterator P1_3

--- P1_3_Iterator/Program.cs
+++ P1_3_Iterator/Program.cs
@@ -1,15 +1,37 @@
-﻿using.System.Collections.IEnumerable
+﻿using System;
+using System.Collections.Generic;
 
-
-List<int> number = new List<int>();
-number.Add(1);
-number.Add(2);
-number.Add(3);
-number.Add(4);
-number.Add(5);
-
-
-public interface IEnumerator
+class Program
 {
-   
+    static void Main()
+    {
+        // Creating a List with 5 numbers, uning Int
+        List<int> numbers = new List<int> { 1, 1, 2, 3, 5 };
+
+        // Assign it to a variable of type IEnumerable
+        IEnumerable<int> enumerableNumbers = numbers;
+
+        // Use GetEnumerator() and a while-loop to print all elements
+        using (var enumerator = enumerableNumbers.GetEnumerator())
+        {
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+
+        // To add up all numbers using IEnumerable and print the sum
+        int sum = 0;
+        foreach (int number in enumerableNumbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine("Sum: " + sum);
+    }
 }
+
+
+
+
+
+
